a05317ef50fc2d1efe9606898a220ea9f362caa7
ADNAN AHMAD <161146546+adnanahmad9907@users.noreply.github.com>
2025-10-01 10:51:29 +0530

    Create Binary_lifting.cpp

---
+++ Binary_lifting.cpp
@@ -0,0 +1,78 @@
+
+class BinaryLifting
+{public:
+    ll timer, L, n;
+    vector<ll> tin, tout;
+    vector<vector<ll>> up, adj;
+    BinaryLifting(vector<vector<ll>> &adj2, ll root = 0)
+    {
+        n = adj2.size();
+        adj = adj2;
+        tin.resize(n);
+        tout.resize(n);
+        timer = 0;
+        L = (ll)log2(n) + 1;
+        up.assign(n, vector<ll>(L + 1, -1));
+        dfs(root, -1);
+    }
+    void dfs(ll u, ll par)
+    {
+        tin[u] = ++timer;
+        up[u][0] = par;
+        for (int i = 1; i <= L; i++)
+        {
+            if (up[u][i - 1] != -1)
+            {
+                up[u][i] = up[up[u][i - 1]][i - 1];
+            }
+            else
+            {
+                up[u][i] = -1;
+            }
+        }
+        for (auto v : adj[u])
+        {
+            if (v != par)
+            {
+                dfs(v, u);
+            }
+        }
+        tout[u] = ++timer;
+    }
+
+    bool is_ancestor(ll u, ll v)
+    {
+        return (tin[u] <= tin[v] && tout[u] >= tout[v]);
+    }
+    ll lca(ll u, ll v)
+    {
+        if (is_ancestor(u, v))
+        {
+            return u;
+        }
+        if (is_ancestor(v, u))
+        {
+            return v;
+        }
+
+        for (int i = L; i >= 0; i--)
+        {
+            if (up[u][i] != -1 && !is_ancestor(up[u][i], v))
+            {
+                u = up[u][i];
+            }
+        }
+        return up[u][0];
+    }
+    ll kth_ancestor(ll u, ll k)
+    {
+        for (ll i = 0; i <= L && u != -1; i++)
+        {
+            if (k & (1LL << i))
+            {
+                u = up[u][i];
+            }
+        }
+        return u;
+    }
+};
