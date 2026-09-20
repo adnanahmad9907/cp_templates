9376cd45ffac0c7465e132fbb86bdb347ad3e8c1
ADNAN AHMAD <161146546+adnanahmad9907@users.noreply.github.com>
2026-09-20 12:26:34 +0530

    Implement line intersection calculation
    Added function to calculate line intersection.

--- geometry.cpp
+++ geometry.cpp
@@ -8,11 +8,26 @@ array<ll, 3> line(pair<ll, ll> p1, pair<ll, ll> p2)
 {
     ll x1 = p1.first, y1 = p1.second;
     ll x2 = p2.first, y2 = p2.second;
+
     ll a = y1 - y2;
     ll b = x2 - x1;
     ll c = x1 * y2 - x2 * y1;
+
+    ll g = __gcd(abs(a), abs(b));
+    g = __gcd(g, abs(c));
+
+    a /= g;
+    b /= g;
+    c /= g;
+
+    if (a < 0 || (a == 0 && b < 0))
+    {
+        a = -a;
+        b = -b;
+        c = -c;
+    }
+
     return {a, b, c};
-    // ax+by+c=0;
 }
 // vec a->b can also take vectors
 pair<ll, ll> vec(pair<ll, ll> p1, pair<ll, ll> p2)
@@ -54,4 +69,17 @@ double pointLineDis(pair<ll, ll> p, array<ll, 3> L)
     return abs((double)A * p.first +
                (double)B * p.second + C) /
            sqrt((double)A * A + (double)B * B);
+}
+
+pair<double, double> lineIntersection(array<ll, 3> l1, array<ll, 3> l2)
+{
+    auto [A1, B1, C1] = l1;
+    auto [A2, B2, C2] = l2;
+
+    double D = (double)A1 * B2 - (double)A2 * B1;
+
+    double x = ((double)B1 * C2 - (double)B2 * C1) / D;
+    double y = ((double)C1 * A2 - (double)C2 * A1) / D;
+
+    return {x, y};
 }
